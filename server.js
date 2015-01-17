8139214953160fd88a391c900eaf0ae25c6e0cee
JuanPablo <jpabloaj@gmail.com>
2015-01-17 09:09:03 -0300

    corregido cambio para fallar

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ site.route('/')
 
 site.route('/api')
   .get(function(req, res){
-    res.json({message:'esta es la API!'});
+    res.json({message:'bienvenido a la API!'});
   });
 
 app.use('/', site);
